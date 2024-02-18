28ec3d563fe3a1cba43821ba703dce70c56fe9ff
GogaImerlishvili <gogaimerlishvili1@gmail.com>
2024-02-18 14:11:14 +0400

    add order info

--- src/components/OrderInfo.jsx
+++ src/components/OrderInfo.jsx
@@ -6,25 +6,15 @@ const OrderInfo = () => {
   return (
     <div className='insturction'>
       <div className='take-pic-instruction'>
+        <div className="change-color">
         <h1 className='take-pic-title'>
         თვალის ფოტოს გადაღების ინსტრუქცია
         </h1>
-        <div className="roman-scroll-order">
-      <div className="scroll-content-order-page" style={{
-          width:"550px",
-          height:"405px",
-          backgroundImage: `url(${paperImg})`,
-          backgroundSize: "cover",
-          // margin:"22px"
-          
-        }}>
-    
        <p> <strong>I.</strong>ვაბნელებთ ოთახს, სანათს ან ტელეფონის ფანარს მიინათებთ თვალის გვერდითა ჭრილიდან, რაც შეიძლება ახლოს მიიტანთ თვალთან რომ კარგად განათდეს, ვიყურებით კამერაში და არ ვიყენებთ კამერის შუქს, ვიღებთ ფოტოს რაც შეიძლება ახლო მანძილიდან.</p>
          <p><strong>II.</strong> თვალის ირისი უნდა ჩანდეს მთლიანად, ეცადეთ ფართოდ გაახილოთ თვალი, ხელის დახმარებაც არ იქნება პრობლემა.</p>
         {/* Add more content as needed */}
 
-      </div>
-    </div>
+        </div>
       </div>
     </div>
   )
